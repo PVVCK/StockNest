aded9b8337994389a648404b2e2a17762bec7138
unknown <chaitanyapvv26@gmail.com>
2025-02-16 22:08:29 +0530

    (feat): Upating Cache for Cart functionality

--- StockNest/src/main/java/com/ecommerce/stocknest/service/cart/CartServiceImpl.java
+++ StockNest/src/main/java/com/ecommerce/stocknest/service/cart/CartServiceImpl.java
@@ -1,12 +1,17 @@
 package com.ecommerce.stocknest.service.cart;
 
 import java.math.BigDecimal;
+import java.util.Arrays;
 import java.util.List;
 import java.util.NoSuchElementException;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cache.annotation.CacheEvict;
+import org.springframework.cache.annotation.CachePut;
+import org.springframework.cache.annotation.Cacheable;
 import org.springframework.stereotype.Service;
 
+import com.ecommerce.stocknest.cache.CachingSetup;
 import com.ecommerce.stocknest.model.Cart;
 import com.ecommerce.stocknest.model.CartItem;
 import com.ecommerce.stocknest.repository.CartItemRepository;
@@ -22,43 +27,22 @@ public class CartServiceImpl implements CartService {
 	
 	@Autowired
 	private CartItemRepository cartItemRepository;
+	
+	@Autowired
+	private CachingSetup cachingSetup;
 
-//	@Override
-//	public Cart getCart(Long id) {
-//		// TODO Auto-generated method stub
-//		 Cart cart = cartRepository.findById(id)
-//				.orElseThrow(()->new NoSuchElementException("Cart with Id:- " +id +" is not Present"));
-//		 
-//		 
-//		BigDecimal totalAmt = cart.getTotalAmout();
-//		cart.setTotalAmout(totalAmt);
-//		return cartRepository.save(cart);
-//	}
-
-//	@Override
-//	public void clearCart(Long id) {
-//		// TODO Auto-generated method stub
-//		
-//		Cart cart = getCart(id);
-//		cartItemRepository.deleteAllByCart_CartId(id);
-//		cart.getCartItems().clear();
-//		cartRepository.deleteById(id);
-//		
-//	}
-
-//	@Override
-//	public BigDecimal getTotalPrice(Long id) {
-//		// TODO Auto-generated method stub
-//		return null;
-//	}
 	
 	@Override
+	@Transactional(rollbackOn = Exception.class)
+	@CacheEvict(value = "Cache_Cart_All", allEntries = true)
     public Cart createCart() {
         Cart cart = new Cart();
         return cartRepository.save(cart);
     }
 
     @Override
+    @Transactional(rollbackOn = Exception.class)
+    @Cacheable(value = "Cache_Cart", key = "#cartId")
     public Cart getCartById(Long cartId) {
         return cartRepository.findById(cartId)
                 .orElseThrow(() -> new NoSuchElementException("Cart not found"));
@@ -66,19 +50,34 @@ public class CartServiceImpl implements CartService {
     
     
     @Override
+    @Transactional(rollbackOn = Exception.class)
+//	@CachePut(value = "Cache_Cart", key = "#cartId")
+//	@CacheEvict(value = "Cache_Product_All", allEntries = true)
     public Cart updateCart(Cart cart) {
         return cartRepository.save(cart);
     }
 
     @Override
-    @Transactional
+    @Transactional(rollbackOn = Exception.class)
+    @CacheEvict(value = "Cache_Cart", key = "#cartId")
     public void deleteCart(Long cartId) {
     	
-        Cart cart = getCartById(cartId); 
-        cartRepository.delete(cart);
+//        Cart cart = getCartById(cartId); 
+//        cartRepository.delete(cart);
+//        
+        cartRepository.findById(cartId)
+        .ifPresentOrElse(user -> {
+        	cartRepository.delete(user);
+            cachingSetup.clearCacheByNames(Arrays.asList("Cache_Cart_All"));
+        }, 
+        () -> {
+            throw new NoSuchElementException("Cart with Id:- " + cartId + " is not Present");
+        });
     }
     
     @Override
+    @Transactional(rollbackOn = Exception.class)
+    @CacheEvict(value = "Cache_Cart", key = "#cartId")
     public void clearCart(Long cartId) {
     	 Cart cart = cartRepository.findById(cartId)
                  .orElseThrow(() -> new RuntimeException("Cart with ID " + cartId + " not found."));
@@ -88,12 +87,16 @@ public class CartServiceImpl implements CartService {
 
          // Reset the total amount in the cart
          cart.setTotalAmount(BigDecimal.ZERO);
+         
 
          // Save the updated cart
          cartRepository.save(cart);
+         cachingSetup.clearCacheByNames(Arrays.asList("Cache_Cart_All"));
     }
 
     @Override
+    @Transactional(rollbackOn = Exception.class)
+    @CachePut(value = "Cache_Cart", key = "#cartId")
     public Cart addItemToCart(Long cartId, CartItem cartItem) {
         Cart cart = getCartById(cartId);
 
@@ -102,11 +105,13 @@ public class CartServiceImpl implements CartService {
 
         // Update total amount
         cart.setTotalAmount(calculateTotalAmount(cart));
-
+        cachingSetup.clearCacheByNames(Arrays.asList("Cache_Cart_All"));
         return cartRepository.save(cart);
     }
     
     @Override
+    @Transactional(rollbackOn = Exception.class)
+    @CachePut(value = "Cache_Cart", key = "#cartId")
     public Cart removeItemFromCart(Long cartId, Long cartItemId) {
         Cart cart = getCartById(cartId);
         CartItem cartItem = cartItemRepository.findById(cartItemId)
@@ -122,6 +127,7 @@ public class CartServiceImpl implements CartService {
     }
     
     @Override
+    @Transactional(rollbackOn = Exception.class)
     public BigDecimal calculateTotalAmount(Cart cart) {
         return cart.getCartItems()
                 .stream()
@@ -131,9 +137,15 @@ public class CartServiceImpl implements CartService {
 
 	@Override
 	@Transactional(rollbackOn = Exception.class)
+	@Cacheable(value = "Cache_Cart_All", key = "'allCarts'")
 	public List<Cart> getAllCarts() {
 		// TODO Auto-generated method stub
-		return cartRepository.findAll();
+		
+		List<Cart> carts = cartRepository.findAll();
+		if (carts.isEmpty()) {
+	        throw new NoSuchElementException("No Carts are available in the database.");
+	    }
+		return carts;
 	}
     
     
